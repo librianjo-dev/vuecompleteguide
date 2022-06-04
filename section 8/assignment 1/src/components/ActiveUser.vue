55842947eaffa39a327ae732efaf98ce457fce53
Paulo Vítor <paulovmedeiros@outlook.com>
2022-06-04 14:12:28 -0300

    assignment finished

---
+++ section 8/assignment 1/src/components/ActiveUser.vue
@@ -0,0 +1,23 @@
+<template>
+  <section>
+    <h2>{{ username }}</h2>
+    <h3>{{ age }}</h3>
+  </section>
+</template>
+
+<script>
+export default {
+    props: {
+        username: {
+            type: String,
+            required: true
+        },
+        age: {
+            type: String,
+            required: true
+        }
+    }
+};
+</script>
+
+
---
+++ section 8/assignment 1/src/components/UserData.vue
@@ -0,0 +1,29 @@
+<template>
+    <section>
+        <form @submit.prevent="submitData">
+            <input type="text" placeholder="Your name" v-model="enteredName" />
+            <input type="text" placeholder="Your age" v-model="enteredAge" />
+            <button>Set user data</button>
+        </form>
+    </section>
+</template>
+
+<script>
+export default {
+    emits: ['set-data'],
+    data () {
+        return {
+            enteredName: '',
+            enteredAge: ''
+        };
+    },
+    methods: {
+        submitData () {
+            this.$emit('set-data', {
+                name: this.enteredName,
+                age: this.enteredAge
+            });
+        }
+    }
+};
+</script>
